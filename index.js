ecc04d20eacc263eff7d2ca732cca75599803ff3
douyemiene <viktormiene.vm@gmail.com>
2022-07-05 22:02:04 +0100

    chore: bring back dotenv

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
 import router from "./router.js";
 
+dotenv.config();
 
 const app = express();
 app.use(express.json())
 
 app.use("/", router);
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+
+
+
+
+
